df72cde66af74502ee14ccbd23f5716b8b6af94b
Carsten Buchberger <c.buchberger@witcom.de>
2021-02-08 12:45:47 +0100

    Backup to S3 for Mysql & Postgresql

--- container-builds/mysql-cloud-backup/resources/perform-backup.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-sed "s/ACCESSKEY/$ACCESS_KEY/g;s/HOSTBASE/$HOSTBASE/g;s/HOSTBUCKET/$HOSTBUCKET/g;s/SECRET/$SECRET/g;s/REGION/$REGION/g;" /s3cfg/s3-template.cfg > /tmp/s3.cfg
-while sleep 3600; do :; done
